eed9e81ad66441bf427aaac7247c953c8f40a6c9
lTimej <lTimej@126.com>
2023-02-18 22:59:16 +0800

    feat:tail日志追踪

--- tailf/tail.go
+++ tailf/tail.go
@@ -1,15 +1,32 @@
 package tailf
 
 import (
+	"github.com/Shopify/sarama"
 	"github.com/hpcloud/tail"
+	"github.com/sirupsen/logrus"
+	"liujun/Time_ELK/etcd"
+	"liujun/Time_ELK/kafka"
 )
 
 var (
-	Tail *tail.Tail
-	err  error
+	err error
 )
 
-func Init(filename string) error {
+type TailTask struct {
+	Path  string     `json:"path"`
+	Topic string     `json:"topic"`
+	Tail  *tail.Tail `json:"tail"`
+}
+
+func NewTailf(msg etcd.EtcdMsg) *TailTask {
+	tail_task := &TailTask{
+		Path:  msg.Path,
+		Topic: msg.Topic,
+	}
+	return tail_task
+}
+
+func (tt *TailTask) Init() (*TailTask, error) {
 	config := tail.Config{
 		ReOpen:    true,
 		Follow:    true,
@@ -17,9 +34,32 @@ func Init(filename string) error {
 		MustExist: false,
 		Poll:      true,
 	}
-	Tail, err = tail.TailFile(filename, config)
-	if err != nil {
-		return err
+	tt.Tail, err = tail.TailFile(tt.Path, config)
+	return tt, err
+}
+
+func (tt *TailTask) Run() {
+	for {
+		line, ok := <-tt.Tail.Lines
+		if ok {
+			logrus.Println(line.Text)
+			msg := &sarama.ProducerMessage{}
+			msg.Topic = tt.Topic
+			msg.Value = sarama.StringEncoder(line.Text)
+			kafka.Msg <- msg
+		}
+	}
+}
+
+func Init(msgs []etcd.EtcdMsg) error {
+	for _, msg := range msgs {
+		tail_task := NewTailf(msg)
+		tail_task, err = tail_task.Init() //根据etcd创建任务
+		if err != nil {
+			return err
+		}
+		//启动任务
+		go tail_task.Run()
 	}
 	return nil
 }
