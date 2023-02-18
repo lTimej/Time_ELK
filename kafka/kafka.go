7f563a16d34beede3ff7f79771c23d3f73b545cd
lTimej <lTimej@126.com>
2023-02-18 22:57:42 +0800

    feat:kafka分布式消息订阅

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -3,14 +3,16 @@ package kafka
 import (
 	"fmt"
 	"github.com/Shopify/sarama"
+	"github.com/sirupsen/logrus"
 )
 
 var (
 	Client sarama.SyncProducer
+	Msg    chan *sarama.ProducerMessage
 	err    error
 )
 
-func Init(addr []string) error {
+func Init(addr []string, capacity int) error {
 	fmt.Println(addr)
 	config := sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.WaitForAll
@@ -20,5 +22,21 @@ func Init(addr []string) error {
 	if err != nil {
 		return err
 	}
+	Msg = make(chan *sarama.ProducerMessage, capacity)
+	go SendMsg()
 	return nil
+}
+func SendMsg() {
+	for {
+		select {
+		case msg := <-Msg:
+			logrus.Println("=========", msg.Topic, msg.Value, "===========")
+			_, _, err := Client.SendMessage(msg)
+			if err != nil {
+				fmt.Println("发送消息失败s, err:", err)
+				return
+			}
+		}
+	}
+
 }
